c9eb4e88f79744a21f959f5c646f1e0eff859429
Antonio de la Rosa <79466355+ant-delarosa@users.noreply.github.com>
2022-06-13 14:25:51 +0200

    Create E_arrays6_intersection_arrays.py
    create file

---
+++ E_arrays6_intersection_arrays.py
@@ -0,0 +1,25 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection. 
+# Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        
+        nums3=[]
+        repeat_found=False
+        
+        for item in nums1:
+
+            for item2 in nums2:
+
+                if (item2==item and repeat_found==False):
+
+                    repeat_found=True
+                    nums3.append(item)
+                    nums2.remove(item2)
+            
+            repeat_found=False
+
+            
+        return nums3
