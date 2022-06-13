adc0510306fec62487c445be20c30273241c4e56
Antonio de la Rosa <79466355+ant-delarosa@users.noreply.github.com>
2022-06-13 14:38:49 +0200

    Update E_arrays6_intersection_arrays.py
    update file

--- E_arrays6_intersection_arrays.py
+++ E_arrays6_intersection_arrays.py
@@ -2,7 +2,29 @@
 # Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 
+#More efficient, using break
 class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+           
+        nums3=[]
+        
+        for item in nums1:
+
+            for item2 in nums2:
+
+                if item2==item:
+                    
+                    nums3.append(item)
+                    nums2.remove(item2)
+                    #we exit from the second for to avoid comparing item again
+                    break
+               
+        return nums3
+
+
+
+#Less efficient
+class Solution2:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         
