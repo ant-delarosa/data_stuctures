3c6eef894fe6483f0813e926599e1aefe355b02b
Antonio de la Rosa <79466355+ant-delarosa@users.noreply.github.com>
2022-06-16 14:05:13 +0200

    Update E_arrays9_two_sum.py

--- E_arrays9_two_sum.py
+++ E_arrays9_two_sum.py
@@ -10,6 +10,23 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+# My second solution, brute force but a bit tweaked
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        res=[]
+        
+        for i in range(0, len(nums)):
+            
+            #dont start by 0, start by i because previous casa are already checked
+            for j in range(i, len(nums)):
+                
+                if nums[i]+nums[j]==target and i!=j:
+                    
+                    #you can return directly the items in a list
+                    return [i, j]
+                     
+        return res
 
 # My first solution, brute force, valid but time limit exceeded
 class Solution2:
@@ -28,13 +45,27 @@ class Solution2:
                     
             if len(res)==2:
                 
-                break
-                    
-                    
-                     
-    
+                break   
         
         return res
       
-      
-      
+   
+# Very efficient solution, complexity les than 0(n^2)
+   class Solution3:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # In this problem, you initialize a dictionary (seen). 
+        # This dictionary will keep track of numbers (as key) and indices (as value). 
+        # when going through the array, you calculate the remaining and check 
+        # to see whether remaining is in the seen dictionary (line #3). 
+        # If it is, you're done! you're current number 
+        # and the remaining from seen would give you the output (line #4). 
+        # Otherwise, you add your current number to the dictionary (line #5) 
+        # since it's going to be a remaining for (probably) 
+        # a number you'll see in the future assuming that there is at least one instance of answer.
+        # T: O(n), S: O(n) for dict extra space
+        seen = dict()
+        for i, value in enumerate(nums): #1
+            remaining = target - nums[i] #2
+
+            if remaining in seen: #3
+                return [i, seen[remaining]]  #4   
