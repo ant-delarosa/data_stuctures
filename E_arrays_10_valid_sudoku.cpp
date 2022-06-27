db093efe7ef984b617a33f2353f6b3b826c099d2
Antonio de la Rosa <79466355+ant-delarosa@users.noreply.github.com>
2022-06-27 12:27:41 +0200

    Update E_arrays_10_valid_sudoku.cpp

--- E_arrays_10_valid_sudoku.cpp
+++ E_arrays_10_valid_sudoku.cpp
@@ -1,8 +1,9 @@
-# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+// IN PROGRESS
+// # Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
-#     Each row must contain the digits 1-9 without repetition.
-#     Each column must contain the digits 1-9 without repetition.
-#     Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// #     Each row must contain the digits 1-9 without repetition.
+// #     Each column must contain the digits 1-9 without repetition.
+// #     Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 
 
 class Solution {
